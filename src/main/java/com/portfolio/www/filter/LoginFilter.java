b18fdd490d598505081223ac525464f4106a9c15
ksuju <139477852+ksuju@users.noreply.github.com>
2024-06-05 15:48:27 +0900

    로그인필터 생성

---
+++ src/main/java/com/portfolio/www/filter/LoginFilter.java
@@ -0,0 +1,91 @@
+package com.portfolio.www.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.util.ObjectUtils;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+/*
+ * @WebFilter(dispatcherTypes = {DispatcherType.REQUEST } , servletNames = {
+ * "pf" })
+ */
+public class LoginFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+	// 생성자
+    public LoginFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+        System.out.println("--------LoginFilter constructor--------");
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+    // 필터 죽으면
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("LoginFilter > destroy");
+	}
+	
+	// 로그인이 필요한 페이지의 URI
+	private final String[] LOGIN_REQUIRED_URI = {
+			"/pf/index.do"
+			//,"/pf/auth/joinPage.do"
+	};
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	// 필터 동작하면
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		String uri = req.getRequestURI();
+		//System.out.println("URI==============================="+uri);
+		List<String> UriList = new ArrayList<String>(Arrays.asList(LOGIN_REQUIRED_URI));
+		//System.out.println("UriList==============================="+UriList);
+		if(UriList.contains(uri)) {
+			HttpSession session = req.getSession();
+			System.out.println("session================>"+session.getAttribute("memberId"));
+			if(ObjectUtils.isEmpty(session.getAttribute("logInUser"))) {
+				resp.sendRedirect(req.getContextPath() + "/auth/loginPage.do");
+	            return;
+			}
+		}
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	// 필터 등록되면
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("LoginFilter > init");
+	}
+
+}
