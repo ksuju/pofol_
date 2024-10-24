387aad258cfe0b6b8c1c8872b68681a80e955a3e
ksuju <139477852+ksuju@users.noreply.github.com>
2024-10-24 17:49:55 +0900

    배포환경 파일경로 수정

--- src/main/java/com/portfolio/www/service/ZipService.java
+++ src/main/java/com/portfolio/www/service/ZipService.java
@@ -8,7 +8,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -27,45 +29,54 @@ public class ZipService {
     Date now = new Date();
     String nowTime = sdf.format(now);
     
-    private String CHECK_PATH = "C:/dev/tmp/" + nowTime;
-	private String SAVE_PATH = "C:/dev/tmp/" + nowTime + "/downAll.zip";
+    private String CHECK_PATH = "/home/ec2-user/files/" + nowTime;
+	private String SAVE_PATH = "/home/ec2-user/files/" + nowTime + "/downAll.zip";
 	
 	// fileNames는 원래 파일 이름 , filePaths는 파일이 저장되어있는 경로
-    public void createZip(List<String> fileNames, List<String> filePaths) throws IOException {
-    	
-    	File destZip = new File(CHECK_PATH);
-    	// 경로 없으면 만들어줌
-        if (!destZip.exists()) {
-        	destZip.mkdirs();
-        }
-    	
-        try (FileOutputStream fout = new FileOutputStream(SAVE_PATH);
-             ZipOutputStream zout = new ZipOutputStream(fout)) {
+	public void createZip(List<String> fileNames, List<String> filePaths) throws IOException {
+	    File destZip = new File(CHECK_PATH);
+	    // 경로가 없으면 생성
+	    if (!destZip.exists()) {
+	        destZip.mkdirs();
+	    }
 
-            for (int i = 0; i < filePaths.size(); i++) {
-                String sourceFile = filePaths.get(i);
-                String originalName = fileNames.get(i);
+	    try (FileOutputStream fout = new FileOutputStream(SAVE_PATH);
+	         ZipOutputStream zout = new ZipOutputStream(fout)) {
 
-                File file = new File(sourceFile);
-                if (file.exists()) {
-                    try (FileInputStream fin = new FileInputStream(file)) {
-                        // 압축 파일의 엔트리 이름을 원래 파일 이름으로 설정
-                        ZipEntry zipEntry = new ZipEntry(originalName);
-                        zout.putNextEntry(zipEntry);
+	        // 처리된 파일 이름을 추적하기 위한 Set (중복된 파일 압축하지 않기 위함)
+	        Set<String> processedFiles = new HashSet<>();
 
-                        byte[] buffer = new byte[1024];
-                        int length;
-                        while ((length = fin.read(buffer)) > 0) {
-                            zout.write(buffer, 0, length);
-                        }
-                        zout.closeEntry();
-                    }
-                } else {
-                    System.err.println("File not found: " + sourceFile);
-                }
-            }
-        }
-    }
+	        for (int i = 0; i < filePaths.size(); i++) {
+	            String sourceFile = filePaths.get(i);
+	            String originalName = fileNames.get(i);
+
+	            // 파일 이름이 이미 처리되었는지 확인
+	            if (processedFiles.contains(originalName)) {
+	                continue; // **중복 파일은 건너뜀**
+	            }
+
+	            File file = new File(sourceFile);
+	            if (file.exists()) {
+	                try (FileInputStream fin = new FileInputStream(file)) {
+	                    // 압축 파일의 엔트리 이름을 원래 파일 이름으로 설정
+	                    ZipEntry zipEntry = new ZipEntry(originalName);
+	                    zout.putNextEntry(zipEntry);
+
+	                    byte[] buffer = new byte[1024];
+	                    int length;
+	                    while ((length = fin.read(buffer)) > 0) {
+	                        zout.write(buffer, 0, length);
+	                    }
+	                    zout.closeEntry();
+	                }
+	                // 이 파일 이름을 처리된 것으로 표시
+	                processedFiles.add(originalName);
+	            } else {
+	                System.err.println("파일을 찾을 수 없습니다: " + sourceFile);
+	            }
+	        }
+	    }
+	}
 
     public void downloadZip(HttpServletResponse response) throws IOException {
     	
